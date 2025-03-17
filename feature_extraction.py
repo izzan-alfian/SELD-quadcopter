82a7b4704fd1397479ebf5d3d350ef664212b74d
Izzan Alfian <93417970+izzan-alfian@users.noreply.github.com>
2025-03-17 15:56:44 +0700

    test1

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,1 +1,7 @@
 import numpy as np
+import librosa
+
+audio_path = "..\datasets\in-flight-source-recording\DREGON_free-flight_speech-high_room1\DREGON_free-flight_speech-high_room1.wav"
+y, sr = librosa.load(audio_path, sr=None)
+
+print(sr)
