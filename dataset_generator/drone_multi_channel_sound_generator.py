7941fce808c6294cb0854d6c07ee83ddaa1902c7
Izzan <izzanalfian1@gmail.com>
2025-05-10 20:56:12 +0700

    New augmented drone noise generator

--- dataset_generator/drone_multi_channel_sound_generator.py
+++ dataset_generator/drone_multi_channel_sound_generator.py
@@ -4,7 +4,10 @@ import matplotlib.pyplot as plt
 from scipy.io import wavfile
 import sounddevice as sd
 
-room = pra.AnechoicRoom(fs=16000, air_absorption=True)
+snr_db = 5.    # signal-to-noise ratio
+distance = np.linalg.norm([3, 3, 3])
+sigma2 = 10**(-snr_db / 10) / (4. * np.pi * distance)**2
+room = pra.AnechoicRoom(fs=16000, air_absorption=True, sigma2_awgn=sigma2)
 
 mic_positions = np.array([
     [ 0.0420,  0.0615, -0.0410],
@@ -19,29 +22,25 @@ mic_positions = np.array([
 
 room.add_microphone_array(mic_positions)
 
-fs, signal = wavfile.read("arctic_a0010.wav")
+fs, signal = wavfile.read("./dataset_generator/arctic_a0010.wav")
 source_position = [3, 3, 3]  # Position of sound source (in meters)
 room.add_source(source_position, signal=signal)
 
 room.compute_rir()
 room.simulate()
 
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')   # or subplot(111) for 2D
-room.plot(ax=ax)
-ax.set_xlim([-5, 5])
-ax.set_ylim([-5, 5])
-ax.set_zlim([-5, 5])
-plt.show()
+# fig = plt.figure()
+# ax = fig.add_subplot(projection='3d')   # or subplot(111) for 2D
+# room.plot(ax=ax)
+# ax.set_xlim([-5, 5])
+# ax.set_ylim([-5, 5])
+# ax.set_zlim([-5, 5])
+# plt.show()
 
-
-sound_signals = room.mic_array.signals
-sound_signals_norm = np.zeros(sound_signals.shape)
-for i, sound in enumerate(sound_signals):
-    sound_signals_norm[i] = sound / np.max(np.abs(sound))
-
-print(sound_signals.shape)
-print(sound_signals_norm.T.shape)
+# sound_signals = room.mic_array.signals
+# sound_signals_norm = np.zeros(sound_signals.shape)
+# for i, sound in enumerate(sound_signals):
+#     sound_signals_norm[i] = sound / np.max(np.abs(sound))
 
 sd.play(sound_signals_norm.T, samplerate=fs) # here
 sd.wait()
