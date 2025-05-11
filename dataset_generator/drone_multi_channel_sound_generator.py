c9cb1e9535d15ca82326282e5c536db225db26f4
Izzan <izzanalfian1@gmail.com>
2025-05-11 23:54:21 +0700

    created new multi channel sound generator

--- dataset_generator/drone_multi_channel_sound_generator.py
+++ dataset_generator/drone_multi_channel_sound_generator.py
@@ -1,13 +1,44 @@
 import numpy as np
 import pyroomacoustics as pra
 import matplotlib.pyplot as plt
-from scipy.io import wavfile
 import sounddevice as sd
+import soundfile as sf
+import librosa
+import os
+from scipy.io import wavfile
 
-snr_db = 5.    # signal-to-noise ratio
-distance = np.linalg.norm([3, 3, 3])
-sigma2 = 10**(-snr_db / 10) / (4. * np.pi * distance)**2
-room = pra.AnechoicRoom(fs=16000, air_absorption=True, sigma2_awgn=sigma2)
+whistles_directory = "./dataset_generator/sounds/whistles"
+motor_noise_directory = "./dataset_generator/sounds/augmented_drone_motor_noises"
+seconds_length = 30
+sr = 44100
+snr = 5
+
+seconds_elapsed = 0
+sound_event_gap = 1.5
+sound_event_gap_tolerant = 0.2
+
+def get_random_3d_pos(radius_outside=100, radius_inside=50):
+    while True:
+        r = radius_outside * np.cbrt(np.random.rand())  # Uniform in volume
+        if r >= radius_inside:
+            break
+
+    theta = np.arccos(2 * np.random.rand() - 1)  # Polar angle (0 to pi)
+    phi = 2 * np.pi * np.random.rand()  # Azimuthal angle (0 to 2pi)
+
+    x = r * np.sin(theta) * np.cos(phi)
+    y = r * np.sin(theta) * np.sin(phi)
+    z = r * np.cos(theta)
+
+    return np.array([x, y, z])
+
+whistle_file_names = os.listdir(whistles_directory)
+whistles_paths = [os.path.join(whistles_directory, f) for f in whistle_file_names]
+
+motor_noise_file_names = os.listdir(motor_noise_directory)
+motor_noise_paths = [os.path.join(motor_noise_directory, f) for f in motor_noise_file_names]
+
+room = pra.AnechoicRoom(fs=sr, air_absorption=True)
 
 mic_positions = np.array([
     [ 0.0420,  0.0615, -0.0410],
@@ -19,28 +50,57 @@ mic_positions = np.array([
     [ 0.0615, -0.0420, -0.0410],
     [ 0.0615,  0.0420,  0.0410],
     ]).T  # Position of microphone
-
 room.add_microphone_array(mic_positions)
 
-fs, signal = wavfile.read("./dataset_generator/arctic_a0010.wav")
-source_position = [3, 3, 3]  # Position of sound source (in meters)
-room.add_source(source_position, signal=signal)
+while True:
+    whistle_path = np.random.choice(whistles_paths)
+    y, sr = librosa.load(whistle_path, mono=False, sr=sr)
+    sound_length = y.shape[-1] / sr
+    random_gap = max(0, np.random.normal(sound_event_gap, sound_event_gap_tolerant))
+    if seconds_elapsed + random_gap + sound_length >= seconds_length:
+        break
+    seconds_elapsed += random_gap
+    source_position = get_random_3d_pos()
+    room.add_source(source_position, signal=y, delay=seconds_elapsed)
+    seconds_elapsed += sound_length
 
 room.compute_rir()
 room.simulate()
 
+for i in motor_noise_paths:
+    y, sr = librosa.load(i, mono=False, sr=sr)
+
+    length_difference = np.abs(y.shape[1] - room.mic_array.signals.shape[1])
+    microphone_signals = np.pad(
+        room.mic_array.signals,
+        ((0,0), (0, (length_difference))),
+        mode='constant',
+        constant_values=0
+    )
+    end_signals = microphone_signals + y
+    break
+
+
 # fig = plt.figure()
-# ax = fig.add_subplot(projection='3d')   # or subplot(111) for 2D
+# ax = fig.add_subplot(projection='3d')
 # room.plot(ax=ax)
-# ax.set_xlim([-5, 5])
-# ax.set_ylim([-5, 5])
-# ax.set_zlim([-5, 5])
+# ax.set_xlim([-100, 100])
+# ax.set_ylim([-100, 100])
+# ax.set_zlim([-100, 100])
 # plt.show()
+
+# sf.write(('./test.wav'), sound_signals.T, sr, subtype='DOUBLE')
 
 # sound_signals = room.mic_array.signals
 # sound_signals_norm = np.zeros(sound_signals.shape)
 # for i, sound in enumerate(sound_signals):
 #     sound_signals_norm[i] = sound / np.max(np.abs(sound))
+# sd.play(sound_signals_norm.T, samplerate=sr) # here
+# sd.wait()
 
-sd.play(sound_signals_norm.T, samplerate=fs) # here
-sd.wait()
+
+# sd.play(end_signals.T, samplerate=sr) # here
+# sd.wait()
+
+sf.write(('./test1.wav'), end_signals.T, sr, subtype='DOUBLE')
+sf.write(('./test2.wav'), microphone_signals.T, sr, subtype='DOUBLE')
