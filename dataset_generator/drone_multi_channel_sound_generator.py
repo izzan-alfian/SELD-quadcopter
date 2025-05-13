27f0b38c09902b21e7582c0435ccedab4b1e9cb2
Izzan <izzanalfian1@gmail.com>
2025-05-13 22:40:02 +0700

    final drone multi channel sound generator

--- dataset_generator/drone_multi_channel_sound_generator.py
+++ dataset_generator/drone_multi_channel_sound_generator.py
@@ -45,11 +45,12 @@ def normalize_sound(sound):
         sound_normalized[i] = y / np.max(np.abs(sound))
     return sound_normalized
 
-def get_random_3d_pos(max_radius=max_possible_whistle_radius, min_radius=min_possible_whistle_radius):
-    while True:
-        r = max_radius * np.cbrt(np.random.rand())  # Uniform in volume
-        if r >= min_radius:
-            break
+def get_random_3d_pos(
+        max_radius=max_possible_whistle_radius,
+        min_radius=min_possible_whistle_radius
+    ):
+    u = np.random.rand()
+    r = ((max_radius**3 - min_radius**3)*u + min_radius**3)**(1/3)
 
     theta = np.arccos(2 * np.random.rand() - 1)  # Polar angle (0 to pi)
     phi = 2 * np.pi * np.random.rand()  # Azimuthal angle (0 to 2pi)
@@ -81,11 +82,11 @@ def simulate_whistle_signals(room):
         whistle_metadata.append(
             {
                 'sound_event_recording': whistle_file_name,
-                'start_time': seconds_elapsed,
-                'end_time': seconds_elapsed + signal_length,
-                'ele': np.arctan2(z, np.sqrt(x**2 + y**2)),
-                'azi': np.arctan2(y, x),
-                'distance': np.sqrt(x**2 + y**2 + z**2)
+                'start_time'           : seconds_elapsed,
+                'end_time'             : seconds_elapsed + signal_length,
+                'ele'                  : np.degrees(np.arctan2(z, np.sqrt(x**2 + y**2))),
+                'azi'                  : np.degrees(np.arctan2(y, x)),
+                'distance'             : np.sqrt(x**2 + y**2 + z**2)
             }
         )
 
@@ -102,10 +103,10 @@ def simulate_whistle_signals(room):
 
 
 whistle_file_names = os.listdir(whistles_directory)
-whistles_paths = [os.path.join(whistles_directory, f) for f in whistle_file_names]
+whistles_file_paths = [os.path.join(whistles_directory, f) for f in whistle_file_names]
 
 motor_noise_file_names = os.listdir(motor_noise_directory)
-motor_noise_paths = [os.path.join(motor_noise_directory, f) for f in motor_noise_file_names]
+motor_noise_file_paths = [os.path.join(motor_noise_directory, f) for f in motor_noise_file_names]
 
 for motor_noise_file_name in motor_noise_file_names:
     print("Generating %s" %motor_noise_file_name)
@@ -125,7 +126,6 @@ for motor_noise_file_name in motor_noise_file_names:
         constant_values=0
     )
 
-    description_file_path = os.path.join(description_result_directory, motor_noise_file_name)
     df = pd.DataFrame({
         'sound_event_recording': [d['sound_event_recording'] for d in whistle_metadata],
         'start_time'           : [d['start_time']            for d in whistle_metadata],
@@ -140,10 +140,32 @@ for motor_noise_file_name in motor_noise_file_names:
     motor_noise *= alpha
 
     microphone_signals = whistle_signals + motor_noise
-    temporary_path = os.path.join(temp_directory, motor_noise_file_name)
 
-    np.save(temporary_path, microphone_signals)
+    temp_file_name = os.path.splitext(motor_noise_file_name)[0]
+    temp_file_path = os.path.join(temp_directory, temp_file_name)
+    np.save(temp_file_path, microphone_signals)
+
+    description_file_name = os.path.splitext(motor_noise_file_name)[0] + ".csv"
+    description_file_path = os.path.join(description_result_directory, description_file_name)
     df.to_csv(description_file_path, index=False)
+
+temp_file_names = os.listdir(temp_directory)
+temp_file_paths = [os.path.join(temp_directory, f) for f in temp_file_names]
+
+max_amplitude = 0
+for temp_file_path in temp_file_paths:
+    temp_file = np.load(temp_file_path)
+    if max_amplitude < np.max(np.abs(temp_file)):
+        max_amplitude = np.max(np.abs(temp_file))
+
+for temp_file_name in temp_file_names:
+    temp_file_path = os.path.join(temp_directory, temp_file_name)
+    temp_file = np.load(temp_file_path)
+    temp_normalized = temp_file / max_amplitude
+
+    wav_file_name = os.path.splitext(temp_file_name)[0] + ".wav"
+    wav_file_path = os.path.join(wav_result_directory, wav_file_name)
+    sf.write(wav_file_path, temp_normalized.T, sr)
 
 
 # fig = plt.figure()
